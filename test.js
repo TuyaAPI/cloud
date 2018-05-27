4df75e4d88c5189458a440141d588e1f48b2dbeb
Max Isom <codetheweb@icloud.com>
2018-05-27 15:40:31 -0500

    Fix Ava errors

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 import test from 'ava';
 
-const Cloud = require('./main.js');
 const apiKeys = require('./keys.json');
+const Cloud = require('.');
 
 const api = new Cloud({key: apiKeys.key, secret: apiKeys.secret});
 
 test('register a new user', async t => {
-  const apiResult = await api.register({email: 'example@example.com',
+  const apiResult = await api.register({email: 'hi@example.com',
                                         password: 'example-password'});
   t.is(apiResult.length, 56);
 });
 
 test('login a user', async t => {
-  const apiResult = await api.login({email: 'example@example.com',
+  const apiResult = await api.login({email: 'hi@example.com',
                                      password: 'example-password'});
   t.is(apiResult.length, 56);
 });
